a44cd20a0ff7700d5b5abfa445d75759b2addd6c
vasi <b.avg@hotmail.com>
2021-09-24 17:10:07 +0200

    added comments

--- form.js
+++ form.js
@@ -1,20 +1,36 @@
 
+/* 
+This Script runs on the client side and dynamically manipulates the HTML content 
+available to the user while also adds hidden inputs to the order table
+in order to be able to send the the table data to the server for further manipulation.
+*/
+
+
+/*Global Scope Variables*/
+// a list containing the total price for each line in the order table
 var singlePrices = [];
+// a simple counter used to dynamically define IDs for rows and specific input elements
 var rowId = 0;
 
 
+/* 
+Function is called everytime the "Add to List" button is pressed.
+it puts the currently selected product, quantity an price in the order table.
+Since this project is for demonstration pusrposes only the prices for each product
+which, normally i.e. could be aquired from a Database, are being randomly generated.
+*/
 function addProductToTable(){
 
     //incremate itemID
     rowId++;
-
+    //get table body 
     var tbodyRef = document.getElementById('order_table').getElementsByTagName('tbody')[0];
 
     // Insert a row at the end of table
     var newRow = tbodyRef.insertRow();
-    //set new rows
-    newRow.id = rowId;
 
+    //set new rows id
+    newRow.id = rowId;
 
     //get cell values as text
     var quantityValue = document.createTextNode(document.getElementById('quantity').value);
@@ -22,17 +38,14 @@ function addProductToTable(){
 
     //generate random value for price
     var randomPrice = generateRandomPrice()
-    var totalPrice = roundToTwo(
-                    parseInt(document.getElementById('quantity').value)
-                    *
-                    parseFloat(randomPrice))
+    var totalPrice = roundToTwo(parseInt(document.getElementById('quantity').value)
+                     * parseFloat(randomPrice))
     var priceValue = document.createTextNode(randomPrice);
     var totalPriceObj = document.createTextNode(totalPrice);
 
-    // Insert a cell at the end of the row
+    // Insert cells to row
     var checkBoxCell = newRow.insertCell();
     var quantityCell = newRow.insertCell();
-    
     var productCell = newRow.insertCell();
     var ppPrice = newRow.insertCell();
     var tpPrice = newRow.insertCell();
@@ -55,7 +68,7 @@ function addProductToTable(){
     tpPrice.innerHTML= "<input type='hidden' name='tpPrice"+rowId+"' value= '"
                             +totalPrice+"'>";
 
-    // set value of quantity cell appearing on form for user                          
+    // set values for cell visible to user of the page                         
     quantityCell.appendChild(quantityValue);
     productCell.appendChild(productText);
     ppPrice.appendChild(priceValue);
@@ -63,22 +76,32 @@ function addProductToTable(){
     
     //append total price to prices list
     singlePrices.push(parseFloat(totalPrice));
-    //console.log(singlePrices);
 
     //reset quantity
     document.getElementById('quantity').value = '1';
+
     //update sum value
     updateSumVal();
 }
 
+/*
+A custom function which rounds a float number to 2 decimals
+ */
 function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
 }
 
+/*
+Generates a random float (price) in a certain range.
+ */
 function generateRandomPrice(){
     return roundToTwo(Math.random() * (500 - 50 + 1));
 }
 
+/*
+Function is called whenever the button in the "Delete Product" column is pressed.
+It removes this row from the table and thus the product from the order.
+*/
 function deleteSelectedItem(callerEle){
     //get the callers id
     var id = callerEle.id;
@@ -104,6 +127,10 @@ function deleteSelectedItem(callerEle){
     rowToRemove.parentNode.removeChild(rowToRemove);
 }
 
+/*
+Function updates the "Sum Total" value after 
+the current sum of the elements in singlePrices list.
+ */
 function updateSumVal(){
     //calculate total sum 
     var sum = roundToTwo(singlePrices.reduce(function(a, b){
@@ -115,8 +142,4 @@ function updateSumVal(){
                             //add hidden input with the total sum for the server
                             "<input type='hidden' name='hiddenSum' value= '"+sum+"'>";
     //set value of hidden input for the server
-    
-    
-    
-    
 }
